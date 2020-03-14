4f7865cb1afb50a98f7bcb17298be419987a5e7b
boikov3 <55747325+boikov3@users.noreply.github.com>
2020-03-14 13:30:14 +0300

    2

--- sources/source.cpp
+++ sources/source.cpp
@@ -24,35 +24,27 @@ using std::rand;
 
 static const int SIZE_ROTATION (5*1024*1024);
 static const int ZERO (0);
-#define WAY "../logs/logFile_%N.log" ;
-#define  FORMAT "[%TimeStamp%]: %Message%" ;
-//static const string WAY ("../logs/logFile_%N.log");
-//static const string FORMAT ("[%TimeStamp%]: %Message%");
 void init_logs() {
     auto log = boost::log::add_file_log(
             boost::log::keywords::file_name = "../logs/logFile_%N.log" ,
             boost::log::keywords::rotation_size = SIZE_ROTATION,
             boost::log::keywords::time_based_rotation =
                     boost::log::sinks::file::rotation_at_time_point{ ZERO, ZERO, ZERO },
-            boost::log::keywords::format = "[%TimeStamp%]: %Message%"
-    );
+            boost::log::keywords::format = "[%TimeStamp%]: %Message%");
 
     auto log2 = boost::log::add_console_log(
             cout,
-            boost::log::keywords::format = "[%TimeStamp%]: %Message%"
-    );
+            boost::log::keywords::format = "[%TimeStamp%]: %Message%");
 
     log->set_filter(
-            boost::log::trivial::severity >= boost::log::trivial::trace
-    );
+            boost::log::trivial::severity >= boost::log::trivial::trace);
     log2->set_filter(
-            boost::log::trivial::severity >= boost::log::trivial::info
-    );
+            boost::log::trivial::severity >= boost::log::trivial::info);
 }
 
 void logs() {
     srand(*(new int));
-    static unsigned int K (0) ;
+    static unsigned int K(0);
 
     while (true) {
         int R = rand_r(&K)%255;
@@ -69,7 +61,7 @@ int main(int argc, char* argv[]) {
      std::cin >> M;
     if (argc >1)
         M = atoi(argv[1]);
-    if (M==0 || M>=12)
+    if (M == 0 || M >= 12)
 
      M = thread::hardware_concurrency();
 
